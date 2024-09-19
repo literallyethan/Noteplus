66f70e1d72beac211c26c163d60a47d9263ae4d0
literallyethan <ethan.k.purser@gmail.com>
2024-09-19 11:36:36 -0500

    noteio.h
    creates an empty module for the purpose of I/O for Noteplus.

---
+++ include/noteio.h
@@ -0,0 +1,13 @@
+#ifndef NOTEIO_H
+#define NOTEIO_H
+
+// Module authored by Ethan Purser
+// Purpose:
+// 	handle writing to, reading from,
+// 	saving, and deleting files
+// 	to and from the local system of the 
+// 	user. 
+
+// Assumes the use of C strings
+
+#endif
