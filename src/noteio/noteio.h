887bab99a5d1dd05e6b4fd1f69f8bec955dd8028
literallyethan <ethan.k.purser@gmail.com>
2024-09-24 11:40:50 -0500

    create_file prototype
    creates a prototype and description of create_file

--- src/noteio/noteio.h
+++ src/noteio/noteio.h
@@ -1,5 +1,8 @@
 #ifndef NOTEIO_H
 #define NOTEIO_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Module authored by Ethan Purser
 // Purpose:
@@ -9,5 +12,12 @@
 // 	user. 
 
 // Assumes the use of C strings
+
+// Assumes a destination folder 
+// already provided in install for storing note files
+
+// Called when making a new note.
+// By default, puts files in the <files> folder. 
+int create_file(const char* path);
 
 #endif
