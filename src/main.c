58d8c41ea4a420d967e971c3921eb5987864ec66
literallyethan <ethanep@victorydev.org>
2024-10-08 20:01:03 -0500

    OpenGL GLEW
    Installs opengl and edits the compile statement to link it to the program.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h> // for glew to work better, idk if necessary -Ethan
 #include <../include/SDL2/SDL.h>
+#include <../include/GL/glew.h>
 
 int main(int argc, char* argv[]) {
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
@@ -9,7 +11,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    
+
 
     SDL_Quit();
     return 0;
