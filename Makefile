79129c88a7f5feec91bff12ca90915a02b6ef553
literallyethan <ethanep@victorydev.org>
2024-10-08 19:44:52 -0500

    SDL install
    Brings the SDL library into the project and a Makefile with the skeleton of the compile statement.

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	gcc -I include -L lib -o main src/main.c -lmingw32 -lSDL2main -lSDL2
+
--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <../include/SDL2/SDL.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        puts("Failed to init SDL.");
+        SDL_GetError();
+        return 1;
+    }
+
     
+
+    SDL_Quit();
     return 0;
 }
