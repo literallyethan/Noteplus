d4c164a15ae5ea4e7995408d83f599ae401ce11e
literallyethan <ethanep@victorydev.org>
2024-10-31 17:01:01 -0500

    deletes useless comments

--- src/main.c
+++ src/main.c
@@ -1,30 +1,3 @@
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h> // for glew to work better, idk if necessary -Ethan
-#include <../include/SDL2/SDL.h>
-#include <../include/GL/glew.h>
-#include "window/windowManager.h"
-
-int main(int argc, char* argv[]) {
-    int running = initialize_window();
-
-    SDL_Event event;
-    while(running) {
-        while(SDL_PollEvent(&event)) {
-            if(event.type == SDL_QUIT) {
-                running = 0;
-            }
-        }
-        glClearColor(1.0f, 0.0, 0.0, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT);
-    }
-
-    
-
-    SDL_Quit();
-    return 0;
-}*/
-
 #include <stdio.h>
 #include <string.h>
 #include "window/windowManager.h"
