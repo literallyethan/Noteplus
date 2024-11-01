5fd61d46e9ac8ddaafb861d03660944ac5fda4f0
literallyethan <43223709+literallyethan@users.noreply.github.com>
2024-11-01 15:10:14 -0500

    SDL_ttf
    Imports SDL_tff library for SDL2 and Titillium font. This will allow us to render text directly onto our window.

--- src/input/input.c
+++ src/input/input.c
@@ -9,6 +9,8 @@ void close_input() {
 }
 
 char* get_input(SDL_Event* event, char buffer[]) {
+    
+    // detects non-printable keys
     if (event->type == SDL_KEYDOWN) {
         if (event->key.keysym.sym == SDLK_BACKSPACE && strlen(buffer) > 0) {
             // Handle backspace: remove the last character
@@ -20,6 +22,7 @@ char* get_input(SDL_Event* event, char buffer[]) {
             buffer[0] = '\0';
         }
     }
+    // detects string input
     else if (event->type == SDL_TEXTINPUT) {
         // Append new text to inputText if it's not full
         if (strlen(buffer) + strlen(event->text.text) < MAX_INPUT_LENGTH) {
